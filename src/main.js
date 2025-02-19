b3e231cb5a6f92f90662db53aaa94aadbb0e0ecc
Francisco Bermudez <bermudezfran15@gmail.com>
2025-02-19 17:23:36 -0300

    last changes

--- src/main.js
+++ src/main.js
@@ -31,27 +31,28 @@ const start = () => {
 };
 
 window.addEventListener('DOMContentLoaded', start);
-
 document.addEventListener('DOMContentLoaded', () => {
-    const themeToggle = document.querySelector('.theme-toggle');
+  const themeToggle = document.querySelector('.theme-toggle');
 
-    if (themeToggle) {
-        
-        if (localStorage.getItem('theme') === 'dark') {
-            document.body.classList.add('dark-theme');
-            themeToggle.textContent = '⚪';
-        }
+  if (themeToggle) {
+      
+      if (localStorage.getItem('theme') === 'dark') {
+          document.body.classList.add('dark-theme');
+          themeToggle.textContent = '⚪';
+      }
 
-        themeToggle.addEventListener('click', () => {
-            document.body.classList.toggle('dark-theme');
+      themeToggle.addEventListener('click', () => {
+          document.body.classList.toggle('dark-theme');
 
-            if (document.body.classList.contains('dark-theme')) {
-                themeToggle.textContent = '⚪';
-                localStorage.setItem('theme', 'dark');
-            } else {
-                themeToggle.textContent = '⚫';
-                localStorage.setItem('theme', 'light');
-            }
-        });
-    }
+          if (document.body.classList.contains('dark-theme')) {
+              themeToggle.textContent = '⚪';
+              localStorage.setItem('theme', 'dark');
+          } else {
+              themeToggle.textContent = '⚫';
+              localStorage.setItem('theme', 'light');
+          }
+      });
+  }
 });
+
+
