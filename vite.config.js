7047388f7fc36e0f15eceb4d23d24584d013b6b5
Francisco Bermudez <bermudezfran15@gmail.com>
2025-02-19 14:34:53 -0300

    se especificaron los estilos

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import {resolve} from 'path' 
+
+export default{
+    css: {
+        devSourcemap: true 
+    },
+    build: {
+        rollupOptions: {
+            input:{
+                main: resolve(__dirname, 'index.html'),
+                contacto: resolve(__dirname, 'src/pages/contacto/contacto.html'),
+                nosotros: resolve(__dirname, 'src/pages/nosotros/nosotros.html')
+            }
+        }
+    }
+}
